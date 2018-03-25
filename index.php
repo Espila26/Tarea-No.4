f85e68da765092be331473b1af70fdecc704a370
Espila26 <epicado2010@gmail.com>
2018-03-25 20:46:48 +0000

    CAMBIOS 2.0

--- index.php
+++ index.php
@@ -4,8 +4,18 @@
 	<title>Cuarta Tarea Programada</title>
 </head>
 <body>
+<html>
+
 <?php
-	
+	$file_db = new POD('sqlite:database.db');
+	 $file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	 function 
 ?>
+	<form>
+		<label></label>
+		<input type="text" name="">
+	</form>
+
 </body>
 </html>
