5104cab0dad704f14bc750221e83e74eb499dc8c
Espila26 <epicado2010@gmail.com>
2018-03-25 19:23:31 -0600

    cambios 4.0

--- index.php
+++ index.php
@@ -7,28 +7,48 @@
 
 <?php
 	try{
-		$file_db = new POD('sqlite:database.db');
+		$file_db = new PDO('sqlite:database.db');
 		$file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 		echo"<form action='".$_SERVER['PHP_SELF']."' method='post' enctype='multipart/form-data'>
-				<label> Fecha: </label>
-				<input type='date' name='date'></input>
-				<label> Cliente: </label>
-				<input type='text' name='client'></input>
-				<label>Impuesto: </label>
-				<input type='text' name='tax'></input>
-				<label> Total: </label>
-				<input type='text' name='Total'></input>
-				<input type='button' type='submit' name='submit'></input>
+				<table>
+					<tr>
+						<td><label> Fecha: </label></td>
+						<td><input type='date' name='date'></input></td>
+					</tr>
+
+					<tr>
+						<td><label> Cliente: </label></td>
+						<td><input type='text' name='client'></input></td>
+					</tr>
+
+					<tr>
+						<td><label>Impuesto: </label></td>
+						<td><input type='text' name='tax'></input></td>
+					</tr>
+
+					<tr>
+						<td><label> Total: </label></td>
+						<td><input type='text' name='Total'></input></td>
+					</tr>
+
+					<tr>
+						<td><input type='button' type='submit' name='submit' value = 'Registrar'></input></td>
+					</tr>
+				</table>	
 			</form>";
-		$resultProduct = $file_db->query('');
-		echo "<table>"
+		$resultProduct = $file_db->query('SELECT * FROM productos');
+		echo "<br />";
+		echo "<br />";
+		echo "<br />";
+		echo "<table>";
 			echo "<tr>";
 				echo "<td>Id</td>";
 				echo "<td>Cantidad</td>";
 				echo "<td>Descripcion</td>";
 				echo "<td>Valor Unitario</td>";
 				echo "<td>Subtotal</td>";
+				echo "<td>Acciones</td>";
 			echo "</tr>";
 			foreach ($resultProduct as $row) {
 				echo "<tr>";
@@ -37,10 +57,10 @@
 					echo "<td>".$row['descripcion']."</td>";
 					echo "<td>".$row['valor-unit']."</td>";
 					echo "<td>".$row['subtotal']."</td>";
-					echo "<td><input type='button' type='submit' name='delete'></input><input type='button' type='submit' name='edit'></input></td>";
+					echo "<td><input type='button' type='submit' name='delete'  value = 'Eliminar'></input><input type='button' type='submit' name='edit'  value = 'Editar'></input></td>";
 				echo "</tr>";
 			}
-		echo "</table>"
+		echo "</table>";
 		$file_db = null;
 	}catch(PDOException $e){
 		echo $e->getMessage();
